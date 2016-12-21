e4077e4e278c383cdf68dfc63e04f3d7ecb5cee4
David <lejonmanen@gmail.com>
2016-12-21 10:23:58 +0100

    Interface 1st version

---
+++ BookingServiceLibrary/BookingServiceLibrary/BookingInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingServiceLibrary
+{
+    [ServiceContract]
+    public interface BookingInterface
+    {
+        [OperationContract]
+        bool MakeBooking(BookingData data);
+
+
+    }
+
+
+}
