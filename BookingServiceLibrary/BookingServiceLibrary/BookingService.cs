82275cddd3fc6d729205b9bae5a4fbf9d7fd1e4a
David Andersson <lejonmanen@gmail.com>
2017-01-03 18:00:54 +0100

    minor edit

--- BookingServiceLibrary/BookingServiceLibrary/BookingService.cs
+++ BookingServiceLibrary/BookingServiceLibrary/BookingService.cs
@@ -14,11 +14,17 @@ namespace BookingServiceLibrary
         public bool MakeBooking(BookingData data)
         {
             // TODO: check if it's possible to make booking
-            Random r = new Random();
-            if (r.Next() % 2 == 0)
+            if (!CheckIfBookingIsPossible(data))
                 return false;
             bookings.Add(data);
             return true;
+        }
+        private bool CheckIfBookingIsPossible(BookingData data)
+        {
+            Random r = new Random();
+            if (r.Next() % 2 == 0)
+                return true;
+            return false;
         }
 
         public IList<BookingData> ViewBookingsFrom(DateTime when)
