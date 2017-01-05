ebf7c82d8150dfa81d2a137a9e763dff21678b3e
David <lejonmanen@gmail.com>
2017-01-05 10:54:04 +0100

    initial

---
+++ BookingMvcClient/BookingMvcClient/Controllers/BookingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookingMvcClient.BookingServiceReference;
+
+namespace BookingMvcClient.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Index()
+        {
+            BookingClient client = new BookingClient();
+            List<BookingData> bookings = 
+                client.ViewBookingsFrom(DateTime.Now);
+            return View(bookings);
+        }
+
+        [HttpGet]
+        public ActionResult BookingForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BookingForm(BookingData formData)
+        {
+            BookingClient client = new BookingClient();
+            client.MakeBooking(formData);
+            return RedirectToAction("Index");
+        }
+    }
+}
